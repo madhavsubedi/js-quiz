a3a51ba8bdb4b1e2951076f7192aa8937a3b4579
Madhav Subedi <madhavsubedi@lftechnology.com>
2016-09-20 16:17:49 +0545

    Adds initial version

---
+++ question-bak.js
@@ -0,0 +1,117 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var question = {
+    'general':
+            {
+                '1': 'Question 1',
+                '2': 'Question 2',
+                '3': 'Question 3',
+                '4': 'Question 4',
+                '5': 'Question 5',
+                '6': 'Question 6',
+                '7': 'Question 7',
+                '8': 'Question 8',
+                '9': 'Question 9',
+                '10': 'Question 10',
+                '11': 'Question 11',
+                '12': 'Question 12',
+                '13': 'Question 13',
+                '14': 'Question 14',
+                '15': 'Question 15',
+                '16': 'Question 16',
+                '17': 'Question 17',
+                '18': 'Question 18',
+                '19': 'Question 19',
+                '20': 'Question 20'
+            },
+    'leaph'://history
+            {
+                '1': "history 1",
+                '2': "history 2"
+            },
+    'leapse'://software engineering
+            {
+                '1': "software engineering 1",
+                '2': "software engineering 2"
+            },
+    'leapst'://science and technology
+            {
+                '1': "science and technology 1",
+                '2': "science and technology 2"
+            },
+    'leaps'://sports
+            {
+                '1': "sports 1",
+                '2': "sports 2"
+            },
+    'leapc'://contemporary
+            {
+                '1': "contemporary 1",
+                '2': "contemporary 2"
+            },
+    'leape'://entertainment
+            {
+                '1': "entertainment 1",
+                '2': "entertainment 2"
+            },
+    'rapid':
+            {
+                '1': 'Rapid fire question 1',
+                '2': 'Rapid fire question 2',
+                '3': 'Rapid fire question 3',
+                '4': 'Rapid fire question 4',
+                '5': 'Rapid fire question 5',
+                '6': 'Rapid fire question 6',
+                '7': 'Rapid fire question 7',
+                '8': 'Rapid fire question 8',
+                '9': 'Rapid fire question 9',
+                '10': 'Rapid fire question 10',
+                '11': 'Rapid fire question 11',
+                '12': 'Rapid fire question 12',
+                '13': 'Rapid fire question 13',
+                '14': 'Rapid fire question 14',
+                '15': 'Rapid fire question 15',
+                '16': 'Rapid fire question 16',
+                '17': 'Rapid fire question 17',
+                '18': 'Rapid fire question 18',
+                '19': 'Rapid fire question 19',
+                '20': 'Rapid fire question 20',
+                '21': 'Rapid fire question 21',
+                '22': 'Rapid fire question 22',
+                '23': 'Rapid fire question 23',
+                '24': 'Rapid fire question 24',
+                '25': 'Rapid fire question 25',
+                '26': 'Rapid fire question 26',
+                '27': 'Rapid fire question 27',
+                '28': 'Rapid fire question 28',
+                '29': 'Rapid fire question 29',
+                '30': 'Rapid fire question 30',
+                '31': 'Rapid fire question 31',
+                '32': 'Rapid fire question 32',
+                '33': 'Rapid fire question 33',
+                '34': 'Rapid fire question 34',
+                '35': 'Rapid fire question 35',
+                '36': 'Rapid fire question 36',
+                '37': 'Rapid fire question 37',
+                '38': 'Rapid fire question 38',
+                '39': 'Rapid fire question 39',
+                '40': 'Rapid fire question 40',
+                '41': 'Rapid fire question 41',
+                '42': 'Rapid fire question 42',
+                '43': 'Rapid fire question 43',
+                '44': 'Rapid fire question 44',
+                '45': 'Rapid fire question 45',
+                '46': 'Rapid fire question 46',
+                '47': 'Rapid fire question 47',
+                '48': 'Rapid fire question 48',
+                '49': 'Rapid fire question 49',
+                '50': 'Rapid fire question 50'
+            }
+};
+
+
+
